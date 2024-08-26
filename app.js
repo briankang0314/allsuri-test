1a6d536cceb76864641fc5fb726e50a6f17bfd25
Byungsoo Kang <briankang0314@gmail.com>
2024-08-27 06:16:53 +0900

    CDN for sendbird import

--- app.js
+++ app.js
@@ -67,8 +67,6 @@ async function ConnectToSendbird(userId) {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js';
 import { getMessaging, getToken } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-messaging.js';
-import SendbirdChat from './node_modules/@sendbird/chat';
-import { GroupChannelModule } from './node_modules/@sendbird/chat/openChannel';
 
 // Global Variables
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
