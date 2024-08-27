3cb737eb4b1e958f6c091bee0f992d1576e3466a
Byungsoo Kang <briankang0314@gmail.com>
2024-08-27 17:15:49 +0900

    just import sendbird cdn in index.html

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ export async function Start()
         if (window.location.pathname === '/oauth/callback') {await LoginByKakao(); return;}
 
         // init sendbird sdk
-        sb = SendbirdChat.init({
-            appId: "9C4825FA-714B-49B2-B75A-72E9E5632578",
-            modules: [
-                new GroupChannelModule(),
-            ],
-        });
+        // sb = SendbirdChat.init({
+        //     appId: "9C4825FA-714B-49B2-B75A-72E9E5632578",
+        //     modules: [
+        //         new GroupChannelModule(),
+        //     ],
+        // });
 
         // check if not configured
         if (localStorage.getItem('user') == null || localStorage.getItem('tokens') == null)
@@ -33,12 +33,12 @@ export async function Start()
             if (!await CheckProfileCompleteness()) {await FillTheBody('my-profile'); ShowIncompleteProfileWarning(); return;}
         }
         
-        try {
-            const user = JSON.parse(localStorage.getItem('user'));
-            await ConnectToSendbird(user.user_id);
-        } catch (error) {
-            console.error('Error connecting to Sendbird:', error);
-        }
+        // try {
+        //     const user = JSON.parse(localStorage.getItem('user'));
+        //     await ConnectToSendbird(user.user_id);
+        // } catch (error) {
+        //     console.error('Error connecting to Sendbird:', error);
+        // }
 
         // Show home
         await FillTheBody('home');
@@ -67,7 +67,6 @@ async function ConnectToSendbird(userId) {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js';
 import { getMessaging, getToken } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-messaging.js';
-import SendbirdChat from "https://cdn.jsdelivr.net/npm/sendbird@3.1.33/SendBird.min.js";
 
 // Global Variables
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
