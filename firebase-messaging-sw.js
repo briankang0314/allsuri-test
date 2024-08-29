1dcba010bcc843b2540b43bfcf7db321ec550b02
Byungsoo Kang <briankang0314@gmail.com>
2024-08-29 15:24:30 +0900

    firebase service worker modified

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,1 @@
-self.addEventListener('push', (event) => {
-	const ThePush = event.data.json().notification;
-	event.waitUntil(
-	  self.registration.showNotification(ThePush.title, {body: ThePush.body})
-	);
-  });
+self.addEventListener('push', (Event) => {ThePush = Event.data.json().notification; Event.waitUntil(self.registration.showNotification(ThePush.title, {body: ThePush.body}));});
